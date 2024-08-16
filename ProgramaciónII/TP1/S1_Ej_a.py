2b3eac95661f36024c5bc80164ca89f89999b077
xtsulyts <531xtsulyts@gmail.com>
2024-08-16 19:04:26 -0300

    subo ej_a seccion_1_A

---
+++ ProgramaciónII/TP1/S1_Ej_a.py
@@ -0,0 +1,23 @@
+"""a. Escribir una función de nombre palabra_no_tiene_letras(palabra,
+letras_prohibidas), la cual retorne True si es que los caracteres que componen
+una palabra no se encuentran en una lista de caracteres prohibidos."""
+
+print("###############")
+print("### ejer. a ###")
+print("###############")
+def palabra_no_tiene_letras(palabra, letras_prohibidas):
+    for letra in palabra:
+        if letra in letras_prohibidas:
+            return False
+    return True
+
+palabra = input("Ingrese una palabra: ")
+letras_prohibidas = ['x', 'y', 'z']
+
+resultado = palabra_no_tiene_letras(palabra, letras_prohibidas)
+
+print(resultado)
+print("Fin de la ejecución")
+
+
+    
