2cc0b63697287e2e0408477c8c007ccac5c9bece
DianaBernigaud <153549855+DianaBernigaud@users.noreply.github.com>
2024-08-17 09:19:49 -0300

    Update S1_Ej_c.py

--- ProgramaciónII/TP1/S1_Ej_c.py
+++ ProgramaciónII/TP1/S1_Ej_c.py
@@ -1,7 +1,14 @@
+'''c. Escriba un procedimiento procesar_palabras(entrada) que acepte una 
+secuencia de palabras separadas por coma, las ordene y las imprima. 
+Suponiendo que la entrada provista al programa es la siguiente:
+te,felicito,que,bien,actuas
+La salida esperada es:
+actuas,bien,felicito,que,te'''
+
 def procesar_palabras(entrada):
     palabras = entrada.split(',')
     palabras_ordenadas = sorted(palabras)
     print(', '.join(palabras_ordenadas))
 
-entrada_usuario = "manzana,pera,banana,uva"
+entrada_usuario = "te,felicito,que,bien,actuas"
 procesar_palabras (entrada_usuario)
