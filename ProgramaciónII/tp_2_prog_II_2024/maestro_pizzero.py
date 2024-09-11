04a1024c9dbc27451450fc231df6174b9ad4ba67
xtsulyts <531xtsulyts@gmail.com>
2024-09-10 23:02:11 -0300

    Avances tp_2 programacion II/fedora

---
+++ ProgramaciónII/tp_2_prog_II_2024/maestro_pizzero.py
@@ -0,0 +1,39 @@
+from pizza import Pizza
+
+class Maestro_pizzero :
+    def __init__(self, nom):
+        self.nombre = nom
+        self.pizzas_por_cocinar = []
+        self.pizzas_por_entregar = []
+
+    def establecer_nombre (self, nombre):
+        self.nombre = nombre
+
+    def tomar_pedido (self, variedad):
+        self.variedad = variedad.Pizza
+        self.pizzas_por_cocinar.append(self.variedad)
+
+    def cocinar (self):
+        self.obtener_pizzas_por_entregar.appen(self.pizzas_por_cocinar)
+
+
+    def entregar ():
+        pass
+
+    def obtener_nombre ():
+        pass
+
+    def obtener_pizzas_por_cocicar ():
+        """Pizza[]"""
+        pass
+    def obtener_pizzas_por_entregar ():
+        """Pizza []"""
+        pass
+
+
+
+
+
+
+
+
---
+++ ProgramaciónII/tp_2_prog_II_2024/pizza.py
@@ -0,0 +1,15 @@
+
+
+class Pizza :
+    def __init__(self, variedad):
+        self.variedad = variedad
+    
+    def establecer_variedad (self, variedad):
+        self.variedad = variedad
+
+
+    def  obtener_variedad ():
+        variedad = "estilos"
+        return self.variedad
+
+
