d977fe5b2c0e412a7453b9620eec0929d18184b0
xtsulyts <531xtsulyts@gmail.com>
2024-10-16 20:50:46 -0300

    agrego obtener estado libre y servir pizzas

--- ProgramaciónII/TP3/mozo.py
+++ ProgramaciónII/TP3/mozo.py
@@ -1,26 +1,17 @@
-class Mozo:
+from pizza import Pizza
 
+class Mozo:
     def __init__(self, nom: str):
-        self.__nombre = nom
-        self.__pizzas = []
+        self.nombre = nom
+        self.pizzas = []
 
     def establecerNombre(self, nom: str):
-        self.__nombre = nom
+        self.nombre = nom
 
-    def tomarPizzas(self, pizzas):
-        pizzasTomadas = len(self.__pizzas)
-        pizzasATomar = len(pizzas)
-        if (pizzasATomar - pizzasTomadas) < 0:
-            print(self.__nombre + ": El mozo puede tomar un máximo de 2 pizzas!")
-        else:
-            for pizza in pizzas:
-                print(self.__nombre + ": tomando una de " + pizza.obtenerVariedad() + " para ser entregada")
-                self.__pizzas.append(pizza)
-    
-    def servirPizzas(self):
-        for pizza in self.__pizzas:
-            print(self.__nombre + ": Sirviendo pizza de " + pizza.obtenerVariedad())
-        self.__pizzas = []
+    def tomarPizzas(self, pizzas: list[Pizza]):
+        if len(pizzas) > 2:
+            raise ValueError("El mozo solo puede tomar 2 pizzas a la vez")
+        self.pizzas = pizzas
 
     def obtenerNombre(self):
         return self.__nombre
@@ -28,6 +19,10 @@ class Mozo:
     def obtenerPizzas(self):
         return self.__pizzas
     
+    def servirPizzas(self):
+        servidas = self.pizzas[:]
+        self.pizzas.clear()
+        return servidas
+
     def obtenerEstadoLibre(self):
-        pizzasTomadas = len(self.__pizzas)
-        return pizzasTomadas < 2
+        return len(self.pizzas) == 0
